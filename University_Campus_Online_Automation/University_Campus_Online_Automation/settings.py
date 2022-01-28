cbf4b950f6ff5964468bfb8f18c31f9d204f4653
poorvi167 <poorvi.gla_cs.ccv19@gla.ac.in>
2022-01-28 13:11:13 +0530

    commit

--- University_Campus_Online_Automation/University_Campus_Online_Automation/settings.py
+++ University_Campus_Online_Automation/University_Campus_Online_Automation/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'Student',
     'Faculty',
     'Visitor',
-    
+
 ]
 
 MIDDLEWARE = [
--- University_Campus_Online_Automation/University_Campus_Online_Automation/urls.py
+++ University_Campus_Online_Automation/University_Campus_Online_Automation/urls.py
@@ -15,16 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/',views.login),
-    path('student/',include('Student.urls')),
-     path('',views.home_page),
-    
-    
+    path('login/', views.login),
+    path('student/', include('Student.urls')),
+    path('', views.home_page),
+
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
